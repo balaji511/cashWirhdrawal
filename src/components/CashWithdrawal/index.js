e8be2500c4956aa8bd025fe3ad9f4b72973c6e28
balaji511 <balaji16f2@gmail.com>
2022-07-01 04:32:42 +0000

    Upload

---
+++ src/components/CashWithdrawal/index.js
@@ -0,0 +1,43 @@
+import {Component} from 'react'
+
+import './index.css'
+import DenominationItem from '../DenominationItem'
+
+class CashWithdrawal extends Component {
+  state = {count: 2000}
+
+  Denominate = value => {
+    console.log(value)
+    this.setState(prevState => ({count: prevState.count - value}))
+  }
+
+  render() {
+    const {count} = this.state
+    const {denominationsList} = this.props
+    return (
+      <div className="bgContainer">
+        <div className="bgCard">
+          <div className="dfs">
+            <h1>Sarah Williams</h1>
+            <p>Your Balance</p>
+            <p>{count} </p>
+          </div>
+          <p>Withdrawal</p>
+          <p>CHOOSE SUM (IN RUPEES)</p>
+          <p>In Rupees</p>
+          <ul className="df">
+            {denominationsList.map(each => (
+              <DenominationItem
+                Details={each}
+                Denominate={this.Denominate}
+                key={each.id}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default CashWithdrawal
